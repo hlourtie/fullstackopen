b1e2366aa73cae6b1b3186d452aaec5a9426be3e
hlourtie <henry.lourtie@gmail.com>
2021-11-23 13:03:34 -0600

    part1exercise7 unicafe part2

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,18 +5,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
   // Components
   const Title = ({name}) => <><h1>{name}</h1></> ;
   const Button = ({handleClick, text}) => <><button onClick={handleClick} >{text}</button></> ;
   const Stats = ({text,value}) => <><p>{text} {value}</p></>
   //Click handlers
   const handleGoodClick = () => {
+    setAll(all + 1);
     setGood(good + 1);
   }
   const handleNeutralClick = () => {
+    setAll(all + 1);
     setNeutral(neutral + 1);
   }
   const handleBadClick = () => {
+    setAll(all + 1);
     setBad(bad + 1);
   }
   //app return
@@ -30,6 +34,9 @@ const App = () => {
       <Stats text="Good" value={good} />
       <Stats text="Neutral" value={neutral} />
       <Stats text="Bad" value={bad} />
+      <Stats text="All" value={all} />
+      <Stats text="Average" value={all !== 0 ?(good - bad)/all : 0} />
+      <Stats text="Positive" value={(all !==0 ? good/all:0)*100+ "%"} />
     </div>
   )
 }
