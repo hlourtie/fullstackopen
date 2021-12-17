3899b00e998cefd4ae1d7a4f2bef41f4e26e9d7e
hlourtie <henry.lourtie@gmail.com>
2021-12-18 00:37:30 +0100

    some form of validation in the phone book entry schema

--- part3/thephonebookbackend/models/entry.js
+++ part3/thephonebookbackend/models/entry.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URI
 
@@ -13,9 +14,20 @@ mongoose.connect(url)
 	})
 
 const entrySchema = new mongoose.Schema({
-	"name": String,
-	"number": String
+	"name":{
+		type:String,
+		minlength:3,
+		required:true,
+		unique:true
+	} ,
+	"number": {
+		type:String,
+		minlength:8,
+		required:true,
+		unique:true
+}
 });
+entrySchema.plugin(uniqueValidator);
 
 entrySchema.set('toJSON', {
 	transform: (document, returnedObj) =>{
